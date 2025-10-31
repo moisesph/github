cff6faa0a027f6587cc3fdcbe1975e69174d6e74
moisesph <mpleytez@byupathway.edu>
2025-10-30 19:47:39 -0600

    affa

--- cse111/project_milestone/tire_volume.py
+++ cse111/project_milestone/tire_volume.py
@@ -1,3 +1,4 @@
+
 import math
 import datetime 
 
@@ -5,23 +6,18 @@ import datetime
 def compute_volume(width, radio, diameter):
     volume = round(math.pi * width ** 2 * radio * (width * radio + 2540 *diameter) / 10000000000,2) #Volume in liters 
     return(volume)
-   
+
+def write_text():
+        with open("volumes.txt", "a") as file:
+            variables = [today, width, radio, diameter, volume]
+            file.write(", ".join(map(str, variables)))
 
 width = float(input("What is the width in mm? "))
 radio = float(input("What is the aspect radio? "))
 diameter = float(input("What is the diameter in inches? "))
 
+today = datetime.datetime.now().date()
+
 
 volume = compute_volume(width, radio, diameter)
-
-
-today = datetime.datetime.now().date()
-print(today)
-
-
-with open("volumes.txt", "a") as file:
-    file.write(str(today) + "," + str(width) + "," + str(radio) + "," + str(diameter) + "," + str(volume))
-
-
-
-
+write_text()
