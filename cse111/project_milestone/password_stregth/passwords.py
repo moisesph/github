a97e7b035f906761a4bfa93d2617731aabb8c4be
moisesph <mpleytez@byupathway.edu>
2025-11-06 18:15:59 -0600

    Homework password completed

--- cse111/project_milestone/password_stregth/passwords.py
+++ cse111/project_milestone/password_stregth/passwords.py
@@ -2,24 +2,10 @@
 Track strength of the password
 """
 
+###🎉✨✨✨ Extra features: in line 39 I added a function to ask user to take off the spaces and in line 98 I called the function
+
 # https://byui-cse.github.io/cse111-ww-course/week02/project.html
 
-
-#5 Calculaled by, password is a regular word? is known password? the length of the password and complexity of the password.
-    #if already is on file, is not secure and strength to 0 "'Password is a dictionary word and is not secure."
-    #If it is in the toppasword list should  "Password is a commonly used password and is not secure." go to 0
-    #if if length less than 10  "Password is too short and is not secure." go to 1
-    #if more than 15, is strong "Password is long, length trumps complexity this is a good password." go to 5
-
-    #4 kinds characters, upper case, lower case, numeric digits, and special symbols, exep, only upper characters, it would be a 1, if it had upper and lower, a 2 etc.
-            #this 4 should be calculaed in base of the length + this four
-
-
-    #Possible for values, should I create a variable where it sums the security from 0 to 1
-
-
-# TO open: open(filename, "r",encoding="utf-8")
-#Remote the new line character at the end of string
 
 
 LOWER=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -33,71 +19,126 @@ def main():
     """
     Loop
     """
-
-    quit = " "
-    while quit != "q":
-        quit = input('Enter password to test it (Press "q" and enter to quit) ').lower()
-
-        if quit != "q":
-            password = quit
-            #print(len(password))
-            password_strength(password)
-        
-        #You need to report result to user
+    global case_sensitive
+    global password
+    case_sensitive = False
+    password = 0
+    word = " "
 
 
-def word_in_file(word,filename,case_sensitive):
+    while word != "q" or quit != "Q":
+        word = input('Enter password to test it (Press "q" and enter to quit) ')
+        print("Password / Strength / Additional Message")
+        if word == "q" or word =="Q":
+            return   
+        password_strength(word)
+
+        return word, password
+    
+    
+def no_space(word):
+    word = list(word)
+    if " " in word :
+        print("Don't enter spaces please\n")
+        main()
+    else: pass
+
+def word_in_file(word, filename): 
     """
      Compare if the name is on file
     """
-    #Read each line on file,
-    for read in files:
-        with open():
-            pass
-    # if word matches, is true, 
-    # other wise is false.
-
-    #if case_sensitive is true, a case sensitive match is performed
-    #if false also
-
-    #Case sensitive should false by default.
-    #Return boolean
-    pass
+    with open(filename, "r",encoding="utf-8") as file:
+        global case_sensitive
+        for line in file:
+            line2 = line.strip()
+            if word in line2:
+                case_sensitive = True
+                break
+            else: 
+                case_sensitive = False
+    return case_sensitive 
 
 def word_has_character(word, character_list):
     """
     Goes through each character in the word parameter and check if matches with any on the character_list, if any character are present returns true, if non false
     """
-    #Boolean
-    pass
+    word = set(word)
+    character_list = set(character_list)
+    if word & character_list:
+        return True
+    else: return False
 
 def word_complexity(word):
     """
     Check how complex is the word
     """
-    # Call word_has_character function, for each 4 kinds characters, 
-    # add to complexity ranking
-    #range from 0 to 4
-    #0 only when no characters
-    #int
-    pass
+    global password
 
-def password_strength(password): #min_length, strong_length
+    if word_has_character(word, LOWER) == True:
+        password += 1
+    if word_has_character(word, UPPER) == True:
+        password += 1
+    if word_has_character(word, DIGITS) == True:
+        password += 1
+    if word_has_character(word, SPECIAL) == True:
+        password += 1
+
+    return password  
+  
+def password_strength(word):
     """
     Computes the final strength
     """
-    #Checks on dictionary and known passwords
-    #call word_complexity
-    #Determine strength based user requirementes
-    #return strength from 0 to 5
-    #the min_length default 10
-    #the strong 16
-    #int
-    pass
+    min_length = 10
+    strong_length = 16
+    global password
+    global case_sensitive
+    password = 0
+   
+    no_space(word)
+    
+    word_in_file(word, "wordlist.txt")
+    if case_sensitive == True:
+        print(f"{word} / {password} / Password is a dictionary word and is not secure.")
+        if case_sensitive == True:
+            main()
+        
+    word_in_file(word, "toppasswords.txt")
+    if case_sensitive == True:
+        print(f"{word} / {password} / Password is a commonly used password and is not secure.")
+        if case_sensitive == True:
+            main()
 
+    word_complexity(word)
+ 
+
+
+    if len(word) >= strong_length and case_sensitive != True:
+        password = 5
+        print(f"{word} / {password} / Password is long, length trumps complexity this is a good password")
+        if case_sensitive != True:
+            main()
+
+
+    elif len(word) >= min_length and len(word) < strong_length and case_sensitive != True:
+        password += 1
+        print(f"{word} / {password} /")
+        main()
+    
+
+    elif password  >= 2 and password < 6 and case_sensitive != True:
+        print(f"{word} / {password} /")
+
+    elif len(word) <= min_length and case_sensitive != True: 
+        print(f"{word} / {password} / Password is too short and is not secure.")
+        if case_sensitive != True:
+            main()
+    
+    elif password == 1 and case_sensitive != True:
+        print(f"{word} / {password} / Password is too short and is not secure.")
+        if case_sensitive != True:
+            main()
+    
 
 if __name__ == "__main__":
     main()
-
-
-#Add an extra feature, put it above everything
