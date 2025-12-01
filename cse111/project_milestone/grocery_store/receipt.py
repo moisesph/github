0e6d173e5a24601cc7d4be632c207b5296c64ca4
moisesph <mpleytez@byupathway.edu>
2025-11-30 18:53:05 -0600

    Grocery

--- cse111/project_milestone/grocery_store/receipt.py
+++ cse111/project_milestone/grocery_store/receipt.py
@@ -1,7 +1,11 @@
+
+###ATTENTION EXTRA FEATURE: I have a counting days for the next black friday in line 78-79 and 89.
+
 """
 Show a receipt with subtotal, tax and total
 """
 import csv
+from datetime import datetime
 
 
 def main():
@@ -10,39 +14,81 @@ def main():
     PRODUCT_INDEX = 0
     NAME_INDEX = 1
     PRICE_INDEX = 2
-    QUANTITY_INDEX_request = 1
+    QUANTITY_INDEX_REQUEST = 1
 
-    products_dict = read_dictionary('products.csv', PRODUCT_INDEX)
-    header = list(products_dict.keys())[0]
-    del products_dict[header]
+    quantity_total = 0
+    price_subtotal = 0
+    item_repeated = 0
+    final_price_subtotal = 0
+
+    tax = 6 / 100
+
+    try:
+
+        products_dict = read_dictionary('products.csv', PRODUCT_INDEX)
+
+        #print("All Products")
+        #print(products_dict)
+        #print(f"Requested Items")
+        print("Out of Stock Store")
+
+        with open("request.csv") as file:
+            first_line = file.readline()
+
+            for line in file:
+                line = line.strip()
+                line_list = line.split(",")
+            
+                name_requested = line_list[PRODUCT_INDEX]
+                quantity_requested = line_list[QUANTITY_INDEX_REQUEST]
+
+             
+                products = products_dict[name_requested]
+                item = products[NAME_INDEX]
+                price = products[PRICE_INDEX]
+                print(f"{item}: {quantity_requested} @ {price}")
+
+                quantity_total += int(quantity_requested)
+                price_subtotal += float(price)
+                item_repeated += int(quantity_requested)
+                final_price_subtotal += float(price) * float(quantity_requested)
 
 
+    except FileNotFoundError:
+        print("Error: missing file")
+        print("[Errno 2] No such file or directory: 'products.csv'")
+        exit()
 
+    except KeyError: 
+        print(f"\nATTENTION! {name_requested} Not found, please contact support\n")
+        exit()
 
+    except PermissionError:
+        print(f"You don't have permission to access")
+        exit()
 
-    print(products_dict)
-
-
-
-    with open('request.csv', 'rt') as file:
-
-        reader = csv.reader(file)
         
-        print(f"Requested Items:")
-        for row in reader:
-            item_requested = row[PRODUCT_INDEX]
-            quantity_requested = row[QUANTITY_INDEX_request]
+    taxes = final_price_subtotal * tax
+    total = final_price_subtotal + taxes   
+
+    now = datetime.now()
+    date = now.strftime("%a %b %d %X %Y")    
+    black_friday = datetime (year=2026, month=11, day=27)
+
+    today = datetime.now()
+    black_friday_difference = black_friday - today      
+        
+    print(f"Number of Items: {quantity_total}")
+    print(f"Subtotal: {final_price_subtotal:.2f} ")
+    print(f"Sales Tax: {taxes:.2f}")
+    print(f"Total: {total:.2f}")
+
+    print("Thank you for shopping at the Out of Stock Store")
+
+    print(date)
+    print(f"Our next black friday is in {black_friday_difference.days} days!")
 
 
-            if item_requested in products_dict:
-        #I'm tryingg to select the values in the products dict 
-                reader2 = csv.reader(products_dict)
-
-                print(f"{item_requested}: {quantity_requested}")
-
-
-
-          
 
 
 
@@ -53,40 +99,16 @@ def read_dictionary(filename, key_column_index):
 
     with open(filename) as file:
         reader = csv.reader(file)
+
+        header = next(reader)
         
         for row in reader:
             iproduct = row[key_column_index]
             dictionary[iproduct] = row
-            
+
     return dictionary
 
 
-
-
-
-##################################
-
-#read products.csv.
-
-
-#Read request.csv
-
-
-
-#see the items in order in the catalog
-
-#calculate and display the order
-
-
-#print store name at the top of receipt
-#Print list ordered items, the name, quantity, and price per item
-#sum and print orders
-#compute and print tax, 6% taxes
-#print total
-#print THanks message
-#Get the date and time from the computer
-
-#Except block to handel FileNotFoundError, PermissionError, and KeyError.
-
-if __name__ in "__main__":
+if __name__ == "__main__":
     main()
+
